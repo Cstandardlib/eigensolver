ad88df3529f95806d933013f2d882e3b20107eb0
Cstandardlib <49788094+Cstandardlib@users.noreply.github.com>
2024-05-02 03:39:11 +0100

    add .gitignore

--- build/timingTest.m
+++
@@ -1,5 +0,0 @@
-function ea = timingTest(A)
-tic
-ea = eigs(A,199,'SM'); % 154.327414 √Î
-toc
-end
--- opensource/utils.f90
+++
@@ -1,9 +0,0 @@
-module utils
-  use real_precision
-  implicit none
-  real(dp), allocatable :: a(:,:), s(:,:), apb(:,:), amb(:,:), aa(:,:), bb(:,:), sigma(:,:), delta(:,:), spd(:,:), smd(:,:)
-  integer               :: nmult, nsmult
-  logical               :: tdscf
-  integer               :: i_alg 
-end module utils
-!
